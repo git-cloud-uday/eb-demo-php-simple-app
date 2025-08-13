13c636234c7606b4144ef01d23174dbb114eb32a
git-cloud-uday <myintellipaat1@gmail.com>
2025-08-13 22:55:21 +0530

    Update deployschema.sh

--- .ebextensions/deployschema.sh
+++ .ebextensions/deployschema.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-set -ex
+DB_HOST="database-1.c0rgyw80mvd0.us-east-1.rds.amazonaws.com"
+DB_USER="admin"
+DB_PASS="Civils1998"
+DB_NAME="database-1"
 
-/usr/bin/mariadb105 \
-    -u $RDS_USERNAME \
-    -p$RDS_PASSWORD \
-    -h $RDS_HOSTNAME \
-    $RDS_DB_NAME \
-    -e 'CREATE TABLE IF NOT EXISTS urler(id INT UNSIGNED NOT NULL AUTO_INCREMENT, author VARCHAR(63) NOT NULL, message TEXT, PRIMARY KEY (id))'
+/usr/bin/mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" < .ebextensions/schema.sql
